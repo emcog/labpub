b679378c16f3ea9282bc87937b6de9409d06fd34
Juan <jpalenzuela@outlook.com>
2020-11-25 12:26:55 +0000

    MVP passed

--- src/customer.py
+++ src/customer.py
@@ -3,3 +3,9 @@ class Customer():
     def __init__(self, name, wallet):
         self.name = name
         self.wallet = wallet
+
+    def buy_drink(self, drink):
+        # 1. reduce the drink amount from customers wallet
+        # 2. increase shop cash by same amount
+        self.wallet -= drink.price
+        pub.cash += drink.price
